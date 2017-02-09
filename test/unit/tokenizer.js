0d259e7e18cba99a983a43ab0f7ba44db2053986
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2017-02-09 10:46:43 +0000

    Added more Tokenizer unit tests to ensure whitespace is handled correctly

--- test/unit/tokenizer.js
+++ test/unit/tokenizer.js
@@ -44,4 +44,36 @@ describe('Tokenizer', () => {
     it('should return array of tokens from external file when the input is \'[@ ... @]\'', () => {
         assert.deepEqual(Tokenizer('[@ test/text.txt @]'), Tokenizer('Hello [[ name ]]!\n'))
     })
+
+    it('should return PLAIN token for exmpty whitespace', () => {
+        assert.deepEqual(Tokenizer(' '), [{ type: K.TOKEN_PLAIN, value: ' ' }])
+    })
+
+    it('should return same EXPRESSION token regardless of whitespace', () => {
+        assert.deepEqual(Tokenizer('[[x]]'), Tokenizer('[[x]]'))
+    })
+
+    it('should return same FOR token regardless of whitespace', () => {
+        assert.deepEqual(Tokenizer('[!for x in xs!]'), Tokenizer('[! for x in xs !]'))
+    })
+
+    it('should return same EFOR token regardless of whitespace', () => {
+        assert.deepEqual(Tokenizer('[!efor!]'), Tokenizer('[! efor !]'))
+    })
+
+    it('should return same IF token regardless of whitespace', () => {
+        assert.deepEqual(Tokenizer('[!if x === 1!]'), Tokenizer('[! if x === 1 !]'))
+    })
+
+    it('should return same ELSEIF token regardless of whitespace', () => {
+        assert.deepEqual(Tokenizer('[!else if y === 2!]'), Tokenizer('[! else if y === 2 !]'))
+    })
+
+    it('should return same ELSE token regardless of whitespace', () => {
+        assert.deepEqual(Tokenizer('[!else!]'), Tokenizer('[! else !]'))
+    })
+
+    it('should return same EIF token regardless of whitespace', () => {
+        assert.deepEqual(Tokenizer('[!eif!]'), Tokenizer('[! eif !]'))
+    })
 })
