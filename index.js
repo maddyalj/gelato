4801d6ef6f5a3bf2e863194033571cc3dd1d20dc
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2017-03-14 09:54:11 +0000

    Implemented multiple file output support

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const vm = require('vm')
 const fs = require('fs-extra')
 const globby = require('globby')
 
@@ -7,10 +8,6 @@ const Evaluator = require('./lib/evaluator')
 
 const Gelato = {}
 
-console.log()
-console.log('Welcome to Gelato! 👋')
-console.log()
-
 Gelato.runOnString = (input, options = { context: {} }, cb = () => {}) => {
     return Tokenizer(input, options, tokens => {
         cb(Evaluator(Parser(tokens), options.context))
@@ -18,10 +15,21 @@ Gelato.runOnString = (input, options = { context: {} }, cb = () => {}) => {
 }
 
 Gelato.run = (options = {}) => {
+    console.log()
+    console.log('Welcome to Gelato! 👋')
+    console.log()
+
     options = Object.assign({
         src: '**/[^_]*.gel',
         dest: 'build',
         context: {},
+        repeat: {},
+        expressionStartTag: '[[',
+        expressionEndTag: ']]',
+        controlStartTag: '[!',
+        controlEndTag: '!]',
+        includeStartTag: '[@',
+        includeEndTag: '@]',
     }, options)
 
     globby(options.src).then(paths => {
@@ -29,16 +37,42 @@ Gelato.run = (options = {}) => {
             console.log(` 👓  Reading ${inputFile}`)
             fs.readFile(inputFile, 'utf8', (err, inputData) => {
                 if (err) throw err
-                const file = `${options.dest}/${inputFile.slice(0, -4)}`
-                Gelato.runOnString(inputData, options, data => {
-                    fs.outputFile(file, data, err => {
-                        if (err) throw err
-                        console.log(` 🍨  Done writing ${inputFile} -> ${file}`)
-                    })
+                Tokenizer(inputData, options, tokens => {
+                    const tree = Parser(tokens)
+                    if (options.repeat[inputFile]) {
+                        repeatOutput(tree, options, inputFile)
+                    } else {
+                        evaluateToFile(tree, options, inputFile)
+                    }
                 })
             })
         })
     })
+
+    function evaluateToFile(tree, options, targetFile, repeatedFile = null) {
+        const outputFile = `${options.dest}/${targetFile.slice(0, -4)}`
+        const output = Evaluator(tree, options.context)
+        fs.outputFile(outputFile, output, err => {
+            if (err) throw err
+            console.log(` 🍨  Done writing ${repeatedFile ? repeatedFile : targetFile} -> ${outputFile}`)
+        })
+    }
+
+    function repeatOutput(tree, options, inputFile) {
+        try {
+            vm.createContext(options.context)
+            const array = vm.runInContext(options.repeat[inputFile].array, options.context)
+            array.forEach(e => {
+                const tempOptions = Object.assign({}, options)
+                tempOptions.context[options.repeat[inputFile].variable] = e
+                Gelato.runOnString(options.repeat[inputFile].filename, tempOptions, file => {
+                    evaluateToFile(tree, tempOptions, `${inputFile.substr(0, inputFile.lastIndexOf('/'))}/${file}`, inputFile)
+                })
+            })
+        } catch (err) {
+            if (err.name !== 'ReferenceError' && err.name !== 'SyntaxError') throw err
+        }
+    }
 }
 
 module.exports = Gelato
