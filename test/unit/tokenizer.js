32391c831cd163b5c2cb7a37f9eae529dbb4ced2
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2017-03-17 23:16:49 +0000

    Added errorLocation to Tokenizer tests

--- test/unit/tokenizer.js
+++ test/unit/tokenizer.js
@@ -16,35 +16,67 @@ describe('Tokenizer', () => {
     })
 
     it('should return PLAIN token when the input is a plain string', () => {
-        assertTokenizer('hello', [{ type: K.TOKEN_PLAIN, value: 'hello' }])
+        assertTokenizer('hello', [{
+            type: K.TOKEN_PLAIN,
+            value: 'hello',
+            errorLocation: '(:1:1)\n                hello\n                ^',
+        }])
     })
 
     it('should return EXPRESSION token when the input is \'[[ ... ]]\'', () => {
-        assertTokenizer('[[ x ]]', [{ type: K.TOKEN_EXPRESSION, value: 'x' }])
+        assertTokenizer('[[ x ]]', [{
+            type: K.TOKEN_EXPRESSION,
+            value: 'x',
+            errorLocation: '(:1:1)\n                [[ x ]]\n                ^',
+        }])
     })
 
     it('should return FOR token when the input is \'[[ for ... ]]\'', () => {
-        assertTokenizer('[! for x in xs !]', [{ type: K.TOKEN_FOR, value: 'x in xs' }])
+        assertTokenizer('[! for x in xs !]', [{
+            type: K.TOKEN_FOR,
+            value: 'x in xs',
+            errorLocation: '(:1:1)\n                [! for x in xs !]\n                ^',
+        }])
     })
 
     it('should return EFOR token when the input is \'[[ efor ]]\'', () => {
-        assertTokenizer('[! efor !]', [{ type: K.TOKEN_EFOR, value: null }])
+        assertTokenizer('[! efor !]', [{
+            type: K.TOKEN_EFOR,
+            value: null,
+            errorLocation: '(:1:1)\n                [! efor !]\n                ^',
+        }])
     })
 
     it('should return IF token when the input is \'[[ if ... ]]\'', () => {
-        assertTokenizer('[! if x === 1 !]', [{ type: K.TOKEN_IF, value: 'x === 1' }])
+        assertTokenizer('[! if x === 1 !]', [{
+            type: K.TOKEN_IF,
+            value: 'x === 1',
+            errorLocation: '(:1:1)\n                [! if x === 1 !]\n                ^',
+        }])
     })
 
     it('should return ELSEIF token when the input is \'[[ else if ... ]]\'', () => {
-        assertTokenizer('[! else if y === 2 !]', [{ type: K.TOKEN_ELSEIF, value: 'y === 2' }])
+        assertTokenizer('[! else if y === 2 !]', [{
+            type: K.TOKEN_ELSEIF,
+            value: 'y === 2',
+            errorLocation: '(:1:1)\n                [! else if y === 2 !]\n                ^',
+        }])
     })
 
     it('should return ELSE token when the input is \'[[ else ]]\'', () => {
-        assertTokenizer('[! else !]', [{ type: K.TOKEN_ELSE, value: null }])
+        assertTokenizer('[! else !]', [{
+            type: K.TOKEN_ELSE,
+            value: null,
+            errorLocation: '(:1:1)\n                [! else !]\n                ^',
+        }])
     })
 
     it('should return EIF token when the input is \'[[ eif ]]\'', () => {
-        assertTokenizer('[! eif !]', [{ type: K.TOKEN_EIF, value: null }])
+        assertTokenizer('[! eif !]', [{
+            type: K.TOKEN_EIF,
+            value: null,
+            errorLocation: '(:1:1)\n                [! eif !]\n                ^',
+        }])
     })
 
     it('should return array of tokens from external file when the input is \'[@ ... @]\'', () => {
@@ -52,7 +84,11 @@ describe('Tokenizer', () => {
     })
 
     it('should return PLAIN token for exmpty whitespace', () => {
-        assertTokenizer(' ', [{ type: K.TOKEN_PLAIN, value: ' ' }])
+        assertTokenizer(' ', [{
+            errorLocation: '(:1:1)\n                 \n                ^',
+            type: K.TOKEN_PLAIN,
+            value: ' ',
+        }])
     })
 
     it('should return same EXPRESSION token regardless of whitespace', () => {
